c2a07f5934d81f448c2988e672cb7bb739cc7d9d
prathmesh <prathmeshpol61@gmail.com>
2024-02-20 18:11:55 +0530

    add - save code funcation added with navigation

--- components/HelperNav.tsx
+++ components/HelperNav.tsx
@@ -1,7 +1,45 @@
+"use client";
+import { useStore } from "@/store/store";
+import { useUser } from "@clerk/nextjs";
 import Link from "next/link";
 import React from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
+interface UserData {
+  createdAt: string; // Assuming ISO 8601 date-time format
+  email: string;
+  fullCode: {
+    css: string;
+    html: string;
+    js: string;
+  };
+  updatedAt: string; // Assuming ISO 8601 date-time format
+  __v: number;
+  _id: string;
+}
 const HelperNav: React.FC = () => {
+  const { user } = useUser();
+  const { css, html, js } = useStore((state) => state);
+  const email = user?.primaryEmailAddress?.emailAddress;
+  const router = useRouter();
+  const saveCode = async () => {
+    try {
+      const res = await axios.post("/api/save-code", {
+        email,
+        fullCode: {
+          html,
+          css,
+          js,
+        },
+      });
+      const data: UserData = await res.data;
+      console.log(data);
+      router.push(`/code/${data._id}`);
+    } catch (error) {
+      console.log("Error white saving code", error);
+    }
+  };
   return (
     <div className="h-[50px] px-12 mx-auto flex justify-between items-center shadow-lg bg-[#0E1525]">
       <Link
@@ -10,12 +48,12 @@ const HelperNav: React.FC = () => {
       >
         CodeEdii
       </Link>
-      <div className="flex items-center gap-4">
-        <button className="bg-green-500 text-white rounded-md text-sm px-4 py-1">
+      <div className="flex items-center">
+        <button
+          className="bg-green-500 text-white rounded-md text-sm px-4 py-1"
+          onClick={saveCode}
+        >
           Save
-        </button>
-        <button className="bg-gray-700 text-white rounded-md text-sm px-4 py-1">
-          Share
         </button>
       </div>
     </div>
